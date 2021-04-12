b3af8372ab042254e3d9ef7d29ee863d861ab7c2
Guy <guyparis94@gmail.com>
2021-04-13 00:39:58 +0300

    Add files via upload

--- cbpi/controller/recipe_controller.py
+++ cbpi/controller/recipe_controller.py
@@ -1,5 +1,5 @@
+
 import logging
-from datetime import datetime
 import os.path
 from os import listdir
 from os.path import isfile, join
@@ -10,17 +10,15 @@ from ..api.step import StepMove, StepResult, StepState
 
 import re
 
-TIME_FORMAT = "%y-%m-%d.%H_%M"
-
-
 class RecipeController:
+
 
     def __init__(self, cbpi):
         self.cbpi = cbpi
         self.logger = logging.getLogger(__name__)
-        self.recipes_by_id = dict()
-
+    
     def urlify(self, s):
+
         # Remove all non-word characters (everything except numbers and letters)
         s = re.sub(r"[^\w\s]", '', s)
 
@@ -29,29 +27,19 @@ class RecipeController:
 
         return s
 
-    def __get_filename(self, ind):
-        name_by_id = self.recipes_by_id.get(ind)
-        if name_by_id:
-            return name_by_id
-        return ind
-
     async def create(self, name):
         id = shortuuid.uuid()
-        time = datetime.now().strftime(TIME_FORMAT)
-        self.recipes_by_id[id] = name + '.' + time
-        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.recipes_by_id[id]))
-        data = dict(basic=dict(name=name, author=self.cbpi.config.get("AUTHOR", "John Doe"), creation_time=time, id=id),
-                    steps=[])
+        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(id))
+        data = dict(basic=dict(name=name, author=self.cbpi.config.get("AUTHOR", "John Doe")), steps=[])
         with open(path, "w") as file:
             yaml.dump(data, file)
         return id
 
     async def save(self, name, data):
-        self.recipes_by_id[name] = data["basic"]["name"] + '.' + data["basic"]["creation_time"]
-        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.recipes_by_id[name]))
+        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(name))
         with open(path, "w") as file:
             yaml.dump(data, file, indent=4, sort_keys=True)
-
+        
         
     async def get_recipes(self):
         path = os.path.join(".", 'config', "recipes")
@@ -59,42 +47,39 @@ class RecipeController:
 
         result = []
         for filename in onlyfiles:
-            recipe_path = os.path.join(".", 'config', "recipes", "{}.yaml".format(filename))
+            recipe_path = os.path.join(".", 'config', "recipes", "%s.yaml" % filename)
             with open(recipe_path) as file:
                 data = yaml.load(file, Loader=yaml.FullLoader)
                 dataset = data["basic"]
-                self.recipes_by_id[dataset["id"]] = dataset["name"] + '.' + dataset["creation_time"]
                 dataset["file"] = filename
                 result.append(dataset)
         return result
-
+    
     async def get_by_name(self, name):
-        recipe_path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.__get_filename(name)))
+        
+        recipe_path = os.path.join(".", 'config', "recipes", "%s.yaml" % name)
         with open(recipe_path) as file:
-            return yaml.load(file, Loader=yaml.FullLoader)
+            return  yaml.load(file, Loader=yaml.FullLoader)
 
+           
     async def remove(self, name):
-        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.__get_filename(name)))
-        with open(path) as file:
-            rec = yaml.load(file, Loader=yaml.FullLoader)
-        rec_name, rec_id = rec["basic"]["name"], rec["basic"]["id"]
+        path = os.path.join(".", 'config', "recipes", "{}.yaml".format(name))
         os.remove(path)
-
+        
 
     async def brew(self, name):
-        recipe_path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.__get_filename(name)))
+
+        recipe_path = os.path.join(".", 'config', "recipes", "%s.yaml" % name)
         with open(recipe_path) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
             await self.cbpi.step.load_recipe(data)
 
-    async def clone(self, name, new_name):
-        recipe_path = os.path.join(".", 'config', "recipes", "{}.yaml".format(self.__get_filename(name)))
+    async def clone(self, id, new_name):
+        recipe_path = os.path.join(".", 'config', "recipes", "%s.yaml" % id)
         with open(recipe_path) as file:
             data = yaml.load(file, Loader=yaml.FullLoader)
             data["basic"]["name"] = new_name
             new_id = shortuuid.uuid()
-            time = datetime.now().strftime(TIME_FORMAT)
-            data["basic"]["id"] = new_id
-            data["basic"]["creation_time"] = time
             await self.save(new_id, data)
+
             return new_id
