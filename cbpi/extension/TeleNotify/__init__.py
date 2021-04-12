db9c26491bdf645f39f67c12515eab8e43382285
Guy <guyparis94@gmail.com>
2021-04-13 02:15:05 +0300

    Add files via upload

--- cbpi/extension/TeleNotify/__init__.py
+++ cbpi/extension/TeleNotify/__init__.py
@@ -1,63 +1,75 @@
-import logging
-import asyncio
+import requests
 from cbpi.api import *
 from cbpi.api.config import ConfigType
-import requests
-
+import logging
+import asyncio
 
 logger = logging.getLogger(__name__)
-
-telegram_token = None
-telegram_user = None
-telegram = None
 
 
 class TeleNotify(CBPiExtension):
 
     def __init__(self, cbpi):
-        self.cbpi = cbpi
+        super().__init__(cbpi=cbpi)
         self._task = asyncio.create_task(self.run())
+        self.token = None
+        self.chat_id = None
+        self.is_init = False
+        self.cbpi = cbpi
+        self.listener_ID = None
+        cbpi.app.on_startup.append(self.register_telegram_notifier)
+        self.cbpi.register(self)
+
+    async def telegram_bot_token(self):
+        self.token = self.cbpi.config.get("TELEGRAM_BOT_TOKEN")
+        if self.token is None:
+            print("INIT Telegram Bot Token")
+            try:
+                await self.cbpi.config.add("TELEGRAM_BOT_TOKEN", "", ConfigType.STRING, "Telegram Bot Token")
+            except:
+                self.cbpi.notify("Telegram Error", "Unable to update database. Update CraftBeerPi and reboot.",
+                                 type="danger")
+
+    async def telegram_chat_id(self):
+        self.chat_id = self.cbpi.config.get("TELEGRAM_CHAT_ID")
+        if self.chat_id is None:
+            print("INIT Telegram Chat ID")
+            try:
+                await self.cbpi.config.add("TELEGRAM_CHAT_ID", "", ConfigType.STRING, "Telegram Chat ID")
+            except:
+                self.cbpi.notify("Telegram Error", "Unable to update database. Update CraftBeerPi and reboot.",
+                                 type="danger")
+
+    @on_startup('reg_telegram', order=9000)
+    async def register_telegram_notifier(self, *args):
+        self.cbpi.app.logger.info("INITIALIZE Telegram PLUGIN")
+        await self.telegram_bot_token()
+        await self.telegram_chat_id()
+        if self.token is None or not self.token:
+            self.cbpi.notify("Telegram", "Check that Telegram Bot Token is set", type="danger")
+        elif self.chat_id is None or not self.chat_id:
+            self.cbpi.notify("Telegram", "Check that Telegram Chat ID is set", type="danger")
+        else:
+            self.is_init = True
 
     async def run(self):
         logger.info('Starting Telegram Notifications background task')
-        await self.TeleUser()
-        await self.TeleToken()
-        if telegram_token is None or telegram_token == "" or not telegram_token:
+        if self.token is None or self.token == "" or not self.token:
             logger.warning('Check Telegram API Token is set')
-        elif telegram_user is None or telegram_user == "" or not telegram_user:
+        elif self.chat_id is None or self.chat_id == "" or not self.chat_id:
             logger.warning('Check Telegram User Key is set')
         else:
             self.listener_ID = self.cbpi.notification.add_listener(self.messageEvent)
             logger.info("Telegram Listener ID: {}".format(self.listener_ID))
-        pass
-
-    async def TeleToken(self):
-        global telegram_token
-        telegram_token = self.cbpi.config.get("telegram_token", None)
-        if telegram_token is None:
-            logger.info("INIT Telegram Token")
-            try:
-                await self.cbpi.config.add("telegram_token", "", ConfigType.STRING, "Telegram API Token")
-            except:
-                logger.warning('Unable to update config')
-
-    async def TeleUser(self):
-        global telegram_user
-        telegram_user = self.cbpi.config.get("telegram_user", None)
-        if telegram_user is None:
-            logger.info("INIT Telegram User Key")
-            try:
-                await self.cbpi.config.add("telegram_user", "", ConfigType.STRING, "Telegram User Key")
-            except:
-                logger.warning('Unable to update config')
+        return
 
     async def messageEvent(self, cbpi, title, message, type, action):
-        tele_data = {"token": telegram_token, "user": telegram_user}
-        text = "<b>" + title + "</b>\n<i>" + message + "</i>"
-        url = "https://api.telegram.org/bot" + tele_data["token"] + "/sendMessage"
-        escaped_url = requests.Request('GET', url, params={"chat_id": tele_data["user"], "text": text,
-                                                           "parse_mode": "HTML"}, ).prepare().url
-        requests.get(escaped_url)
+        if self.is_init:
+            text = "<b>" + title + "</b>\n<i>" + message + "</i>"
+            url = "https://api.telegram.org/bot" + self.token + "/sendMessage"
+            escaped_url = requests.Request('GET', url, params={"chat_id": self.chat_id, "text": text,
+                                                               "parse_mode": "HTML"}, ).prepare().url
+            requests.get(escaped_url)
 
 
 def setup(cbpi):
